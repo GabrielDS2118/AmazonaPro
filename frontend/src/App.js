6a0f4974b040d88b98f77f4032f871e6ec0d9da1
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-11-01 14:22:46 -0500

    1.Configuracion desde cero

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona pro</a>
+      </header>
+      <main>Lista de productos</main>
+    </div>
+  );
+}
+
+export default App;
