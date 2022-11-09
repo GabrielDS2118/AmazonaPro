084c9daccf11fa89bbd4b5efe8836309d2b2caba
Gabriel Aristizabal <gabrielhotm2020@outlook.com>
2022-11-08 20:33:12 -0500

    SigIn API

--- backend/data.js
+++ backend/data.js
@@ -11,7 +11,7 @@ const data = {
     {
       name: 'John',
       email: 'user@example.com',
-      password: bcrypt.hashSync('123456'),
+      password: bcrypt.hashSync('789101'),
       isAdmin: false,
     },
   ],
